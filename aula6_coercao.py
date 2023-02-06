45100c2533b44fd47d1532d64d8e296460224557
Vinicius Sibin <vinisibim@gmail.com>
2023-02-06 15:06:28 -0300

    Feito aula6 sobre coerçao de dados

---
+++ aula6_coercao.py
@@ -0,0 +1,11 @@
+"""
+    Conversão de tipos, coerção
+    type convertion, typecastring, coercion é o ato de converter um tipo em outro.
+    Tipos imutaveis e primitivos: str, int, float, bool 
+"""
+
+#Para converter uma string em inteiro basta utilizar a função int(), float(), str() ou bool()
+print(int('1') + 1)
+print(float('1') + 1)
+print(str(2) + 'a')
+print(bool('f'))
