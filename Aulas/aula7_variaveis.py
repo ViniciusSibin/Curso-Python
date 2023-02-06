b53efcf833357b11b7e39bdd89bab3a10cf8ab31
Vinicius Sibin <vinisibim@gmail.com>
2023-02-06 15:23:17 -0300

    Feito aula 7 sobre variáveis

---
+++ Aulas/aula7_variaveis.py
@@ -0,0 +1,17 @@
+# Variáveis são utilizadas para salvar algum dado na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar números e underline '_'.
+# O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a uma variável.
+#Exemplo de uso: nome_variavel = expressão.
+
+teste_variavel = 'expresssão de teste'
+
+print(teste_variavel)
+
+
+nome = 'Vinicius Luiz Silva Sibin'
+idade = 24
+de_maior = idade >= 18
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('É de maior?', de_maior)
