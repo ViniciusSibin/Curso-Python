584483a19e5ff0339b9d5ea55f29d46e70b0518b
Vinicius Sibin <vinisibim@gmail.com>
2023-02-06 16:42:42 -0300

    Finalizado aula 9 sobre operadores aritmeticos

---
+++ Aulas/aula9_operadoresAritmeticos.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('Adição: 10+10 = ', adicao)
+
+subtracao = 10 - 5
+print('Subtração: 10-5cls =', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação: 10*10 =', multiplicacao)
+
+divisao = 10 / 2.2 #sempre o retorno é float
+print('Divisão: 10/2.2 =', divisao)
+
+divisao_inteira = 10 // 2
+print('Divisão inteira: 10//2.2 =', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponencição: 2**10 =', exponenciacao)
+
+modulo = 55 % 2 #resto da divisão
+print('Módulo: 55%2 =', modulo)
