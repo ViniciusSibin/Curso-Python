0e29034d0049a00649eaf1cde6a11b1f91860f52
Vinicius Sibin <vinisibim@gmail.com>
2023-02-06 15:46:23 -0300

    Feito aula 8 de exercicios sobre variáveis

---
+++ Aulas/aula8_exercicioVariaveis.py
@@ -0,0 +1,13 @@
+nome = 'Vinicius'
+sobrenome = 'Sibin'
+idade = 24
+ano_nascimento = 1998
+maior_de_idade = idade >= 18
+altura = str(1.68)
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura:', altura + 'm')
