3f32d7f6730788bfc49ee3ea5f9f4368d55c7c84
Vinicius Sibin <vinisibim@gmail.com>
2023-02-15 08:22:31 -0300

    Alterado a extensão do exercicio para .py

--- Exercicios/aula12_CalculoIMC.py
+++ Exercicios/aula12_CalculoIMC.py
@@ -23,11 +23,3 @@ elif imc < 39.9:
     print('Sua classificação é: OBESIDADE II')
 else:
     print('Sua classificação é: OBESIDADE GRAVE III')
-
-# MENOR QUE 18,5	MAGREZA	0
-# ENTRE 18,5 E 24,9	NORMAL	0
-# ENTRE 25,0 E 29,9	SOBREPESO	I
-# ENTRE 30,0 E 39,9	OBESIDADE	II
-# MAIOR QUE 40,0	OBESIDADE GRAVE	III 
-
-
