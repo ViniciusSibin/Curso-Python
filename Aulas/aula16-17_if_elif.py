d800911c080651c3247e846f6a71efb1376abedc
Vinicius Sibin <vinisibim@gmail.com>
2023-02-24 12:47:47 -0300

    Feito aula 17, mais sobre if e elif

--- Aulas/aula16-17_if_elif.py
+++ Aulas/aula16-17_if_elif.py
@@ -3,9 +3,9 @@
 
 entrada = input('Você quer "entrar" ou "sair" ?')
 
-if entrada == 'entrar':
+if entrada == 'entrar': #Se a condição for verdadeira executa a linha abaixo
     print('Você entrou no sistema')
-elif entrada == 'sair':
+elif entrada == 'sair': #executa se a primeira condição não for verdadeira mas essa sim
     print('Você saiu do sistema')
-else:
+else: # executa se nenhuma condição for verdadeira
     print('Você digitou uma opção invalida')
