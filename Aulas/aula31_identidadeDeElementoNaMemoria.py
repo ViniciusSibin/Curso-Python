d3fd0b95ed25a666d58c2d3de913b1e0316011d5
Vinicius Sibin <vinisibim@gmail.com>
2023-05-10 17:00:59 -0300

    Finalizado aula 31 sobre identidade de algum valor em memória

---
+++ Aulas/aula31_identidadeDeElementoNaMemoria.py
@@ -0,0 +1,20 @@
+"""
+    Flag (Bandeira) - Marcar um local
+    None = Não valor
+    is e is not = é ou não é (tipo, valor, identidade)
+    id = identidade
+"""
+
+v1 = 'a'
+v2 = 'a'
+print(id(v1)) 
+print(id(v2)) # Caso tenha alguma outra variável com o mesmo valor já salvo na memória, o Sistema Operacional irá reutilizar a mesma identidade na memória 
+
+# condicao = False
+
+"""
+if condicao:
+    print('Faça algo')
+else:
+    print('Não faça algo')
+"""
