b8a25a927a97815d392817182646221541385205
Vinicius Sibin <vinisibim@gmail.com>
2023-05-19 10:54:35 -0300

    Feito aula 34 sobre while e break

--- Aulas/aula34_while.py
+++ Aulas/aula34_while.py
@@ -2,4 +2,14 @@
     Repetições
     while (enquanto)
     Executa uma ação enquanto uma ação for verdadeira.
+    Loop infinito --> Quando um código não tem fim, executa constantemente. 
 """
+
+condicao = True
+
+while condicao:
+    nome = input('Escreva alguma coisa: ')
+    print(f'nome escrito: {nome}')
+
+    if nome == "sair": #Enquanto o usuário não digitar sair, o loop não para de executar.
+        break
