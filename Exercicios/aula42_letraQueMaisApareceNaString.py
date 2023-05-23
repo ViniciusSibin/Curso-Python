8eb6b61a9baec8b3e5fe3696f40de9ff9d2ecedc
Vinicius Sibin <vinisibim@gmail.com>
2023-05-23 17:58:38 -0300

    Feito exercicio da aula 42, contagem de caracteres em uma string

---
+++ Exercicios/aula42_letraQueMaisApareceNaString.py
@@ -0,0 +1,21 @@
+"""
+    Criar um script para verificar qual caractere apareceu mais vezes na frase.
+"""
+frase = 'O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.aaaaaaaaaaaaaaaaawwwwwwwwwwwwwwwwwwwwwww'
+i = 0
+letra_final = ''
+qtd_caracteres_final = 0
+
+while i < len(frase):
+    qtd_caracteres = frase.count(frase[i])
+    print(f'{qtd_caracteres=}',f'{frase[i]=}')
+
+    if qtd_caracteres > qtd_caracteres_final:
+        qtd_caracteres_final = qtd_caracteres
+        letra_final = frase[i]
+    elif qtd_caracteres == qtd_caracteres_final:
+        if frase[i] not in letra_final:
+            letra_final += ', ' + frase[i]
+
+    i += 1
+print(f'A letra que mais apareceu foi: {letra_final} por {qtd_caracteres_final} vezes!!!')
