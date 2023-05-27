18a56a83829706a0b5f5e314a32c137659f9da57
Vinicius Sibin <vinisibim@gmail.com>
2023-05-26 22:24:15 -0300

    Arrumado exercicio da aula 42 para não contar os espaços

--- Exercicios/aula42_letraQueMaisApareceNaString.py
+++ Exercicios/aula42_letraQueMaisApareceNaString.py
@@ -1,7 +1,7 @@
 """
     Criar um script para verificar qual caractere apareceu mais vezes na frase.
 """
-frase = 'O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.aaaaaaaaaaaaaaaaawwwwwwwwwwwwwwwwwwwwwww'
+frase = 'O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.'
 i = 0
 letra_final = ''
 qtd_caracteres_final = 0
@@ -9,7 +9,7 @@ qtd_caracteres_final = 0
 while i < len(frase):
     qtd_caracteres = frase.count(frase[i])
 
-    if qtd_caracteres > qtd_caracteres_final:
+    if qtd_caracteres > qtd_caracteres_final and frase[i] != ' ':
         qtd_caracteres_final = qtd_caracteres
         letra_final = frase[i]
     elif qtd_caracteres == qtd_caracteres_final:
